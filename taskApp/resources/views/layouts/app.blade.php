95285af59635beee5c049fe82707e51a3b3e5a10
bmoore639 <142362933+bmoore639@users.noreply.github.com>
2025-02-18 21:26:51 +0000

    first commit

---
+++ taskApp/resources/views/layouts/app.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset = "UTF-8" />
+	<meta name="viewport" content ="width=device-width, initial-scale=1.0"/>
+	<title> Laravel Task App</title>
+	@vite(['resources/css/app.css', 'resources/js/app.js'])
+
+</head>
+
+<body class ="bg-gray-100">
+	<nav class="bg-white shadow p-4">
+	<!--navbar content -->
+	<h1 class-"text-xl font-bold">My task App</h1>
+</nav>
+<main class="p-4">
+@yield('content')
+</main>
+
+</body>
+</html>
+
+
